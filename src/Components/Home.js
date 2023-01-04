ac63d13be7e5d81905ca095818daaa16c9709fb3
Tushar <tusharvamanamdoskar@gmail.com>
2023-01-04 21:00:38 +0530

    14

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,8 @@
 function Home(){
     return(
         <div className="home">
-            Home
+            <br/>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/qKofi7l0bzU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     )
 }
